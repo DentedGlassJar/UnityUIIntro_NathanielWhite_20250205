4ae7853c0224d5e9728f296f383f74200d486f93
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-02-05 19:28:17 -0400

    Submarine Added + Text Work Begun

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ScoreUpdate()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
