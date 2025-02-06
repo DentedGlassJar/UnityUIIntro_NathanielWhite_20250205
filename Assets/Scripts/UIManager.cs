916f6251708e64f718acad346b23475a7cc34ddd
DentedGlassJar <nathanielwhite867991@gmail.com>
2025-02-05 20:02:30 -0400

    Third Image Implemented

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-
-    private void Update()
-    {
-        
-    }
-
+    private int score;
+    public TextMeshProUGUI scoreOutput;
     public void ScoreUpdate(int value)
     {
-
+        //scoreOutput = ;
     }
 }
